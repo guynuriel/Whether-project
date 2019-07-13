8b0ce8d75850b15cb6687188e7de8ee84187b53d
גיא נוריאל <nurielguy@gmail.com>
2019-07-13 15:30:16 +0300

    555

--- server.js
+++ server.js
@@ -3,10 +3,8 @@ var app = express()
 var path = require('path')
 var mongoose = require('mongoose')
 const api = require('./server/api')
-
 mongoose.connect(process.env.MONGODB_URI ||"mongodb://localhost/places", { useNewUrlParser: true })
 app.use(express.static(path.join(__dirname, 'dist')))
-
 var bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
@@ -14,7 +12,7 @@ app.use('/', api)
 
 
 
-const port = process.env.port|| 3000
+const port = process.env.PORT|| 3000
 app.listen(port, function() {
     console.log(`Server up and running on port ${port}`)
   })
