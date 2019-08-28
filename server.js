0f77df1a02261d8448ecd923773b1b5caf7035e5
גיא נוריאל <nurielguy@gmail.com>
2019-08-28 09:52:09 +0300

    fix bug

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
-var express = require('express')
-var app = express()
-var path = require('path')
-var mongoose = require('mongoose')
+const express = require('express')
+const path = require('path')
+const mongoose = require('mongoose')
 const api = require('./server/api')
-mongoose.connect(process.env.MONGODB_URI ||"mongodb://localhost/places", { useNewUrlParser: true })
+const app = express()
+const bodyParser = require('body-parser')
 app.use(express.static(path.join(__dirname, 'dist')))
-var bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use('/', api)
 
+mongoose.connect(process.env.MONGODB_URI ||"mongodb://localhost/places", { useNewUrlParser: true })
 
 
 const port =  3000
